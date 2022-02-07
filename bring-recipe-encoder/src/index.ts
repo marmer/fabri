057863c61d226378aff41d7cc4703217ad870313
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-08 00:17:24 +0100

    recipe Producer api

--- bring-recipe-encoder/src/index.ts
+++
@@ -1,2 +0,0 @@
-const bark = () => 'bark'
-export { bark }
--- bring-recipe-provider/functions/src/index.ts
+++ bring-recipe-provider/functions/src/index.ts
@@ -1,42 +1,17 @@
-import * as functions from "firebase-functions";
-
+import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
 export const recipe = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  // Import URL Sample
-  // https://api.getbring.com/rest/bringrecipes/deeplink?url=https%3A%2F%2Fus-central1-bring-recipe-provider.cloudfunctions.net%2Frecipe&source=web&baseQuantity=4&requestedQuantity=4
-  response.contentType("application/json")
+  response.contentType('application/json')
       .status(200)
       .send(JSON.stringify({
-        author: "fabri - Fast Bring Recipe Importer",
-        linkOutUrl: "https://fabri.marmer.online",
-        // imageUrl: "https://www.myrecipe.com/recipeImage.jpg",
-        name: "Fancy Nancies Feuernudeln",
-        // tagline: "",
-        // yield: "",
-        // time: "",
-        // nutrition: {
-        //   calories: "500 kcal",
-        // },
-        items: [{
-          itemId: "Karotten",
-          spec: "2-3",
-        }, {
-          itemId: "Hähnchenkeulen",
-          spec: "4",
-        }, {
-          itemId: "Mehl",
-          spec: "250g",
-        }, {
-          itemId: "Sumach",
-          spec: "1/4 TL",
-          altIcon: "Rosmarin",
-          altSection: "Früchte & Gemüse",
-        }, {
-          itemId: "Salz",
-          spec: "1 TL",
-        }],
+        author: 'fabri - Fast Bring Recipe Importer',
+        linkOutUrl: 'https://fabri.marmer.online',
+        name: request.params[0].substring(1),
+        items: Object.entries(request.query).map(([itemId, spec]) => ({
+          itemId,
+          spec: spec || undefined,
+        })),
       }));
 });
