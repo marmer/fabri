7833a18b3214292c3587cdb3f53b0de5b3c89fd6
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-10 13:45:59 +0100

    newly and freshly generated app

--- fabri-app/build/publish.ts
+++ fabri-app/build/publish.ts
@@ -6,12 +6,12 @@ const git: SimpleGit = simpleGit();
 
 (async () => {
   await fs.promises.copyFile('dist/index.html', 'dist/404.html')
-  await fs.promises.rm('docs', {
+  await fs.promises.rm('../docs', {
     recursive: true,
     force: true
   })
   await fs.promises.rename('dist', '../docs')
-  git.add('docs', err => {
+  git.add('../docs', err => {
     if (err) {
       console.error(err)
     }
