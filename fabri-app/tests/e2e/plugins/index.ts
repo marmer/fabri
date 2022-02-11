9891b4cc34f5cc37e5aa284c3ba53409deeb8a67
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-11 16:23:53 +0100

    typescript für Cypress

--- fabri-app/tests/e2e/plugins/index.ts
+++ fabri-app/tests/e2e/plugins/index.ts
@@ -9,17 +9,18 @@
 // /* eslint-disable import/no-extraneous-dependencies, global-require */
 // const webpack = require('@cypress/webpack-preprocessor')
 
-module.exports = (on, config) => {
+export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
   // on('file:preprocessor', webpack({
   //  webpackOptions: require('@vue/cli-service/webpack.config'),
   //  watchOptions: {}
   // }))
 
-  return Object.assign({}, config, {
+  return {
+    ...config,
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
     screenshotsFolder: 'tests/e2e/screenshots',
     videosFolder: 'tests/e2e/videos',
-    supportFile: 'tests/e2e/support/index.js'
-  })
+    supportFile: 'tests/e2e/support/index.ts'
+  }
 }
