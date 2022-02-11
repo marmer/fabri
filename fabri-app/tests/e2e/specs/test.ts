638de10769fc322904fd04410fb7769403f47347
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-11 16:31:23 +0100

    cypress testing library

--- fabri-app/tests/e2e/specs/test.ts
+++ fabri-app/tests/e2e/specs/test.ts
@@ -4,5 +4,8 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.findByText('About')
+      .click()
+    cy.findByText('This is an about page')
   })
 })
