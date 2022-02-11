4d1063dd351332a59760d2fe2c0e4248ad412c9a
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-11 22:12:13 +0100

    tryout

---
+++ docs/js/about.87f917c6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfabri"]=self["webpackChunkfabri"]||[]).push([[443],{4335:function(n,a,r){r.r(a),r.d(a,{default:function(){return b}});var u=r(3396);const t={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),s=[e];function i(n,a){return(0,u.wg)(),(0,u.iD)("div",t,s)}var c=r(89);const f={},o=(0,c.Z)(f,[["render",i]]);var b=o}}]);
+//# sourceMappingURL=about.87f917c6.js.map
