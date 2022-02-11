f0d737e192870cace1efc2c302b4dfbbe56a7722
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-11 16:53:46 +0100

    vue 3 upgrade

--- fabri-app/src/components/HelloWorld.vue
+++ fabri-app/src/components/HelloWorld.vue
@@ -37,16 +37,20 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Options, Vue } from 'vue-class-component'
 
-@Component
+@Options({
+  props: {
+    msg: String
+  }
+})
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  msg!: string
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style scoped>
 h3 {
   margin: 40px 0 0;
 }
