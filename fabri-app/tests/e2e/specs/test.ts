66879a227a26e993b3b9c53ce1fabe81cb449676
mariano.mertinat <Mariano.Mertinat@gematik.de>
2022-02-21 14:56:05 +0100

    e2e acceptance test

--- fabri-app/tests/e2e/specs/test.ts
+++ fabri-app/tests/e2e/specs/test.ts
@@ -1,11 +1,52 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Recipe-Creation', () => {
+  it('First Site visit', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
-    cy.findByText('About')
-      .click()
-    cy.findByText('This is an about page')
+    cy.title()
+      .should('eq', 'fabri')
+    cy.contains('h1', 'Fast bring recipe importer')
+    cy.url()
+      .should('not.contain', 'n=')
+
+    cy.contains('h2', 'Recipe')
+      .next()
+      .within(() => {
+        cy.findByPlaceholderText('Recipe Name')
+          .should('be.empty')
+          .type('Humus mit Erdbeeren')
+
+        cy.url()
+          .should('contain', `n=${encodeURIComponent('Humus mit Erdbeeren')}`)
+
+        cy.findByPlaceholderText('ingredient 1')
+          .should('be.empty')
+          .type('Kichererbsen')
+
+        cy.url()
+          .should('contain', `n=${encodeURIComponent('Humus mit Erdbeeren')}`)
+          .should('contain', `${encodeURIComponent('Kichererbsen')}`)
+
+        cy.findByPlaceholderText('amount 1')
+          .should('be.empty')
+          .type('200g')
+
+        cy.url()
+          .should('contain', `n=${encodeURIComponent('Humus mit Erdbeeren')}`)
+          .should('contain', `${encodeURIComponent('Kichererbsen=200g')}`)
+
+        cy.findByPlaceholderText('ingredient 2')
+          .should('be.empty')
+          .type('Öl')
+
+        cy.findByPlaceholderText('amount 2')
+          .should('be.empty')
+          .type('70ml')
+
+      // TODO: marmer 21.02.2022 Import/Export with a nice url (and a redirect Check!. Don't forget to mock the getbring api here!)
+      })
   })
+
+  // TODO: marmer 21.02.2022 Number of portions
+  // TODO: marmer 21.02.2022 Visit with ingredients
 })
