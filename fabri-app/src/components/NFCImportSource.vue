ae7a6380c6fb6ac54f3003b80a4db8659f536748
MarMer <1055445+marmer@users.noreply.github.com>
2022-02-26 00:31:58 +0100

    nfc tryout

--- fabri-app/src/components/NFCImportSource.vue
+++ fabri-app/src/components/NFCImportSource.vue
@@ -1,18 +1,31 @@
 <template>
   <h2>NFC Import</h2>
-  <button v-if="isNFCCapable" @click="doScan()">scan</button>
+  <div v-if="isNFCCapable">
+    <button @click="doScan()">scan</button>
+    <button @click="doWrite()">scan</button>
+  </div>
   <div v-else>Your device does not know anything about NFC</div>
 </template>
 
 <script lang="ts" setup>
 
-import { computed } from 'vue'
+import { computed, defineProps } from 'vue'
+import { Recipe } from 'fabri-core/lib/types'
+import { encode as encodeRecipe } from 'fabri-core'
+/* global NDEFReader */
 
 const isNFCCapable = computed(() => 'NDEFReader' in window)
+const props = defineProps<{
+  recipe: Recipe
+}>()
+
+const recipeProviderURL = computed(() =>
+  `https://us-central1-bring-recipe-provider.cloudfunctions.net/recipes/${encodeRecipe(props.recipe)}`
+)
+
+const bringImportUrl = computed(() => `https://api.getbring.com/rest/bringrecipes/deeplink?url=${encodeURIComponent(recipeProviderURL.value)}&source=web&baseQuantity=4&requestedQuantity=4`)
 
 const doScan = () => {
-  /* global NDEFReader */
-
   const ndef = new NDEFReader()
 
   ndef.scan().then(() => {
@@ -21,11 +34,23 @@ const doScan = () => {
       alert('Cannot read data from the NFC tag. Try another one?')
     }
     ndef.onreading = event => {
-      alert('NDEF message read.')
+      event.message.records.forEach(it => alert(JSON.stringify(it)))
     }
   }).catch(error => {
     alert(`Error! Scan failed to start: ${error}.`)
   })
+}
+
+const doWrite = async () => {
+  const ndef = new NDEFReader()
+
+  try {
+    await ndef.write({
+      records: [{ recordType: 'url', data: bringImportUrl.value }]
+    }, { overwrite: true })
+  } catch (error) {
+    alert(`Error! Scan failed to start: ${error}.`)
+  }
 }
 
 </script>
